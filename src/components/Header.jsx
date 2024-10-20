0d0ccba6b75af3ae1d6dc81ab2e926f26f346df2
davidalantodd <107488171+davidalantodd@users.noreply.github.com>
2024-10-20 19:58:35 -0400

    add dropdown functionality to hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import chickadee from '../assets/chickadee.png'
+import { Dropdown } from 'react-bootstrap'
 
 function Header() {
     return (
@@ -8,9 +9,17 @@ function Header() {
                 <h1 className="title">chickadee</h1>
             </a>
             <div className="spacer"></div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-list menu-icon" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-            </svg>
+            <Dropdown>
+                <Dropdown.Toggle className="menu-dropdown e-caret-hide" variant="nooutline" show="false">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-list menu-icon" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                </Dropdown.Toggle>
+                <></>
+                <Dropdown.Menu variant="nooutline">
+                    <Dropdown.Item href="/">Observations by Region</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </nav>
     )
 }
